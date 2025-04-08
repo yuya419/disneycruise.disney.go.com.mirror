9094e997a24e60fdc01a25ab018fca3ec1d265d8
Yuya <89628621+yuya419@users.noreply.github.com>
2025-04-08 16:09:32 +0900

    [update] ■ Basic認証 - middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,29 +1,26 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextResponse } from 'next/server';
 
-export function middleware(req: NextRequest) {
-    const basicAuth = req.headers.get("authorization");
+export function middleware(req: Request) {
+    const basicAuth = req.headers.get('authorization');
+
+    // Production環境のみ適用
+    if (process.env.NODE_ENV !== 'production') {
+        return NextResponse.next();
+    }
 
     if (basicAuth) {
-        const authValue = basicAuth.split(" ")[1];
-        const [user, password] = atob(authValue).split(":");
+        const auth = basicAuth.split(' ')[1];
+        const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':');
 
-        // ユーザー名とパスワードを検証
-        if (user === "red" && password === "red") {
-            return NextResponse.next(); // 認証成功
+        if (user === process.env.BASIC_AUTH_USER && pwd === process.env.BASIC_AUTH_PASSWORD) {
+            return NextResponse.next();
         }
     }
 
-    // 認証失敗時のレスポンス
-    return new NextResponse("Authentication required", {
+    return new NextResponse('Authentication required', {
         status: 401,
         headers: {
-            "WWW-Authenticate": 'Basic realm="Secure Area"',
+            'WWW-Authenticate': 'Basic realm="Secure Area"',
         },
     });
 }
-
-// 適用するパスを指定
-export const config = {
-    matcher: ["/ships/adventure((?!/api|/_next/static|/_next/image|/favicon.ico).*)"], // basePathを考慮
-};
